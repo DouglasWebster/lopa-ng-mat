33bf430ca1e02e045eb6e78479d179cb0ade69ea
DouglasWebster <douglas.webster2107@gmail.com>
2017-06-25 12:31:12 +0100

    Reworked to enable basic test compliance.

--- src/app/admin/welcome/welcome.component.spec.ts
+++ src/app/admin/welcome/welcome.component.spec.ts
@@ -1,16 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MaterialModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { Observable } from 'rxjs/Observable';
+import { Subscriber} from 'rxjs/Subscriber';
+
+import { User } from '../../shared/models';
+import { UserService } from '../../shared/services';
 
 import { WelcomeComponent } from './welcome.component';
 
 describe('WelcomeComponent', () => {
   let component: WelcomeComponent;
   let fixture: ComponentFixture<WelcomeComponent>;
+  const fakeUsers = '[{"firstName":"Micky","lastName":"Mouse","userName":"mMouse",'
+    + '"password":"Password1","admin":true,"id":1},{"firstName":"Daffy","lastName":"Duck",'
+    + '"userName":"dDuck","password":"Password2","admin":false,"id":2}]'
+
+  const userServiceStub = {
+    getAll(): Observable<String[]> {
+      return new Observable<String[]>((subscriber: Subscriber<String[]>) => subscriber.next(JSON.parse(fakeUsers)));
+    }
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WelcomeComponent ]
+      imports: [
+        MaterialModule,
+        RouterTestingModule
+      ],
+      declarations: [WelcomeComponent],
+      providers: [
+        { provide: UserService, useValue: userServiceStub }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
